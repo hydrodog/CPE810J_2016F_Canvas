47cb34b533308fedf580a978f4c53161945ea172
binglinxie <bxie3@stevens.edu>
2016-11-11 12:42:20 -0500

    use this file to test

--- Grade.java
+++ Grade.java
@@ -1,3 +1,16 @@
+/* 
+ * To post grade to canvas
+ * Informations you should provide:
+ *	courseId 
+ *	assignmentId;
+ *	studentId;
+ *	token;
+ *	grade;
+ *  call Grade.updateGrade() to update the grade
+ *  to do: support more type of grade, for example : ABC , 1-5, 1-100....;
+ *         finish comment method;
+ *  
+ */
 package update;
 
 import java.io.*;
@@ -43,14 +56,15 @@ public class Grade {
     	this.token=token;
     	this.grade=grade;
 	}
-	public void updateGrade(String courseId,String assignmentId,String studentId,String token,double grade){
+	public void updateGrade(){
     	Grade.targetURL = "https://canvas.instructure.com/api/v1"
     			+ "/courses/"+this.courseId
     			+ "/assignments/"+this.assignmentId
     			+ "/submissions/update_grades"
     			+ "?access_token="
     			+ this.token
-    			+ "&grade_data["+this.studentId+"[posted_grade]="+this.grade;
+    			+ "&grade_data["+this.studentId+"[posted_grade]="+this.grade
+    			+ "&grade_data["+this.studentId+"]"+"[text_comment]=hello!";
     	try {
 
 			URL targetUrl = new URL(targetURL);
@@ -88,6 +102,16 @@ public class Grade {
 		 }
 
 		}
+	public static void main(String[] args){//a test example
+	    String courseId="10300000000000133";
+	    String assignmentId="10300000000046026";
+	    String userId="10300000000022347";
+	    String token="1030~BxcmBG0xRZw720NNodMrxAH9FIA7HlH9T48yi0I5SDLqzJc4tmoMsWiSS9tMx36S";
+	    Grade test = new Grade(courseId,assignmentId,userId,token,6);
+	    //test.printurl();
+	    test.updateGrade();
+	    
+   }
     	
     }
 	
