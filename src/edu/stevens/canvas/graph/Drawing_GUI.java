8048200bc56c4a758e4e0ea7ee72504a879ca52e
changlan1030 <lchang1@stevens.edu>
2016-11-16 18:18:31 -0500

    Update
    make some little change

--- src/edu/stevens/canvas/graph/Drawing_GUI.java
+++ src/edu/stevens/canvas/graph/Drawing_GUI.java
@@ -1,6 +1,9 @@
-package edu.stevens.canvas.graph;
-
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.*;
 
 /**
@@ -10,13 +13,15 @@ import javax.swing.*;
  */
 
 public class Drawing_GUI extends JFrame {
-	// private Graph_GUI gui;
-	private boolean allStudent = true;
-	private boolean allAssignment = true;
+	private boolean allStudent;
+	private boolean allAssignment;
 	private String assignmentTypeChoosen;
 	
-	public Drawing_GUI() {  // Graph_GUI call this constructor
-		//this.gui = ui;
+	public Drawing_GUI(boolean allStudent, boolean allAssignment, String assignmentTypeChoosen) {
+		this.allStudent = allStudent;
+		this.allAssignment = allAssignment;
+		this.assignmentTypeChoosen = assignmentTypeChoosen;
+		
 		this.setTitle("Drawing GUI");
 		this.setSize(1500, 1000);
 		this.setResizable(false);
@@ -27,29 +32,19 @@ public class Drawing_GUI extends JFrame {
 		title.setHorizontalAlignment(SwingConstants.CENTER);
 		c.add(title, BorderLayout.NORTH);
 		
-		DrawingArea d = new DrawingArea(this);
+		DrawingArea d = new DrawingArea(allStudent, allAssignment, assignmentTypeChoosen);
 		c.add(d, BorderLayout.CENTER);
-		this.setVisible(true);
 		
-		// need to get allStudent, allAssignment, assignmentTypeChoosen
-		// this.allStudent = ui.getAllStudent();
-		// this.allAssignment = ui.getAllAssignment();
-		// this.assignmentTypeChoosen = ui.getAssignmentTypeChoosen();
-	}
-	
-	public boolean getAllStudent() {
-		return allStudent;
-	}
-	
-	public boolean getAllAssignment() {
-		return allAssignment;
-	}
-	
-	public String getAssignmentTypeChoosen() {
-		return assignmentTypeChoosen;
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+		
+		this.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
-		new Drawing_GUI();
+		new Drawing_GUI(true, false, "assignment2");
 	}
 }
