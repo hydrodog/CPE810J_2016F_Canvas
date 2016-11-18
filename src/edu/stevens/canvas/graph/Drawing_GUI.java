190e8e3dd8d0cd957cd13bef2e69e1233bd625f9
changlan1030 <lchang1@stevens.edu>
2016-11-18 13:26:48 -0500

    Update Drawing_GUI.java

--- src/edu/stevens/canvas/graph/Drawing_GUI.java
+++ src/edu/stevens/canvas/graph/Drawing_GUI.java
@@ -1,11 +1,7 @@
 package edu.stevens.canvas.graph;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
+import java.awt.event.*;
 import javax.swing.*;
 
 /**
