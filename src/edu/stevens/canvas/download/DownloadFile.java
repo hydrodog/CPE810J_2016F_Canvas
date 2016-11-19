f861096433156fa591734a6af18ef8359fa12aba
gchentech <gchen13@stevens.edu>
2016-11-19 14:29:50 -0500

    change url, still works
    find assignment url is tough, i ask them in Canvas, still not work.
    https://community.canvaslms.com/message/55682

--- src/edu/stevens/canvas/download/DownloadFile.java
+++ src/edu/stevens/canvas/download/DownloadFile.java
@@ -20,7 +20,7 @@ public class DownloadFile {
   public static void main(String[] args) throws IOException {
 		 
 		 String fileName = "a.csv"; //The file that will be saved on your computer
-		 URL link = new URL("https://instructure-uploads.s3.amazonaws.com/account_10300000000000001/attachments/2198944/ArduinoUno_R3_Front.jpg?AWSAccessKeyId=AKIAJFNFXH2V2O7RPCAA&Expires=1479666403&Signature=KSqS1m6fpe0CelmxbZzsN7GadTY%3D&response-content-disposition=attachment%3B%20filename%3D%22ArduinoUno_R3_Front.jpg%22%3B%20filename%2A%3DUTF-8%27%27ArduinoUno%255FR3%255FFront.jpg");
+		 URL link = new URL("https://canvas.instructure.com/files/1030~2198944/download?download_frd=1&verifier=GQiL2ORgUV663l63jChXHabqj1syAYcqSHQVtm1D");
 				 ReadableByteChannel rbc = Channels.newChannel(link.openStream());
 		 FileOutputStream fos = new FileOutputStream("01.jpg");
 		 fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
