07837c736ca252d2c8f9fe902d4e71df15966ede
unknown <>
2016-11-19 12:06:26 -0500

    add python

---
+++ PlagiarismDetector/src/Compile/CompilePython.java
@@ -0,0 +1,11 @@
+package Compile;
+
+public class CompilePython implements Compile{
+
+	@Override
+	public void compileSourceCode() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
