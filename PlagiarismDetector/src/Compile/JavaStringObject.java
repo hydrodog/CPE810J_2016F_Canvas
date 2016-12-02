fa77c78f8c30b4b810775d03d7b44a3b27f4bf3a
supengyuislearning <supengyumail@gmail.com>
2016-12-02 12:33:22 -0500

    a new method of DynamicCompile

---
+++ PlagiarismDetector/src/Compile/JavaStringObject.java
@@ -0,0 +1,16 @@
+package Compile;
+
+import java.io.IOException;
+import java.net.URI;
+import javax.tools.SimpleJavaFileObject;
+
+public class JavaStringObject extends SimpleJavaFileObject{
+	private String code;
+	public JavaStringObject(String name, String code){
+		super(URI.create(name + ".java"), Kind.SOURCE);
+		this.code = code;
+	}
+	public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException{
+		return code;
+	}
+}
