85c3a1fab60277dfd29a25bcd52f8ed3fc184a36
supengyuislearning <supengyumail@gmail.com>
2016-12-04 22:26:13 -0500

    12042016

--- PlagiarismDetector/src/binaryCodeCompare/JavaClassReaderTest.java
+++ PlagiarismDetector/src/binaryCodeCompare/JavaClassReaderTest.java
@@ -3,8 +3,7 @@ package binaryCodeCompare;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+
 
 public class JavaClassReaderTest {
 	public static void main(String[] args) throws Exception{
