609d7c428629fbba493a2f3ff201570676140989
supengyuislearning <supengyumail@gmail.com>
2016-12-04 20:57:07 -0500

    12042016 get every byte in java class file

--- PlagiarismDetector/src/binaryCodeCompare/JavaClassReaderTest.java
+++ PlagiarismDetector/src/binaryCodeCompare/JavaClassReaderTest.java
@@ -7,17 +7,17 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class JavaClassReaderTest {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws Exception{
 		File f = new File("e:/temp/HelloWorld.class");
 		FileInputStream input = null;
 			input = new FileInputStream(f);
 		DataInputStream data = new DataInputStream(input);
-		StringBuilder b = new StringBuilder();
-		String s;
-		while((s = data.readLine()) != null){
-			b.append(s);
+		StringBuilder s = new StringBuilder();
+		while(data.available() > 0){
+			s.append(data.readByte()+ "\n");
 		}
-		System.out.println(b);
+		System.out.println(s);
+		System.out.println(s.length());
 	}
 	
 	
