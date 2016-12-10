b8f903ac583ec2cf40441cf9697c0283adade202
supengyuislearning <supengyumail@gmail.com>
2016-12-10 13:16:13 -0500

    Use runtime.exec() to complete CompileCpp class

---
+++ PlagiarismDetector/src/compile/CompileCppTest.java
@@ -0,0 +1,11 @@
+package compile;
+
+import java.io.File;
+
+public class CompileCppTest {
+	public static void main(String[] args) {
+		File f = new File("e:HelloWorld.cpp");
+		CompileCpp c = new CompileCpp(f);
+		c.compileSourceCode();
+	}
+}
