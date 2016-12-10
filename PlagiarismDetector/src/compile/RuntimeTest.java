c9e6f57c5aec250d7eca6e875f578d26d070da09
supengyuislearning <supengyumail@gmail.com>
2016-12-10 11:06:23 -0500

    Complete RuntimeTest. Save before integrate into compile.Compilecpp

--- PlagiarismDetector/src/compile/RuntimeTest.java
+++ PlagiarismDetector/src/compile/RuntimeTest.java
@@ -12,15 +12,12 @@ public class RuntimeTest {
 		
 		try {
 			//String[] s = {"vcvarsall.bat","e:","cl/EHscHelloWorld.cpp"};
-			Runtime.getRuntime().exec("vcvarsall.bat").waitFor();
-			
-			Runtime.getRuntime().exec("cl/EHscHelloWorld.cpp").waitFor();
-		;
+			Process p1 = Runtime.getRuntime().exec("cmd /c vcvarsall.bat&&e:&&cl /EHsc HelloWorld.cpp");
 			
 			String line;
 			System.out.println("yes");
 			
-			/*BufferedReader br = new BufferedReader(new InputStreamReader(p1.getErrorStream()));
+			BufferedReader br = new BufferedReader(new InputStreamReader(p1.getErrorStream()));
 			BufferedReader br1 = new BufferedReader(new InputStreamReader(p1.getInputStream()));
 			while ( (line = br.readLine()) != null)
 			     System.out.println(line);
@@ -29,12 +26,9 @@ public class RuntimeTest {
 			while ( (line = br1.readLine()) != null)
 			     System.out.println(line);
 			System.out.println("fail");
-			br.close();*/
+			br.close();
          System.exit(1);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
