a767bdadbe5c062d549507092b4bf5c204bea750
yucheng xie <yxie14@stevens.edu>
2016-12-14 22:13:30 -0500

    update location of program

--- src/edu/stevens/canvas/automaticHW/Compile.java
+++ src/edu/stevens/canvas/automaticHW/Compile.java
@@ -38,7 +38,7 @@ class Compile extends JFrame {
         run = new JButton("run");
         run.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                run();
+                runCpp();
             }
         });
         graLab = new JLabel("Grade");
@@ -67,118 +67,123 @@ class Compile extends JFrame {
         this.setBounds(300, 100, 650, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-//    public void run() {
-//        String sss = "";
-//        try {
-//            Process pro = Runtime.getRuntime().exec("javac Main.java"); //need to modify to get the path of the file.
-//            String line = null;
-//            BufferedReader in = new BufferedReader(
-//                    new InputStreamReader(pro.getErrorStream()));
-//            while ((line = in.readLine()) != null) { //display the error location of compiling
-//                rstDis.append(line + "\r\n");
-//                // System.out.println("javac Main.java" + " The error : " + " " + line);
-//            }
-//            pro.waitFor();
-//            int exitValue = pro.exitValue();
-//
-//                // System.out.println("javac Main.java" + " exitValue() " + pro.exitValue()); //show "0" if compile successfully
-//
-//            if (exitValue == 1) {
-//                sss = Integer.toString(25);
-//                grade(sss);
-//                gra.setText(sss);
-//            } else {
-//                pro = Runtime.getRuntime().exec("java Main");
-//                line = null;
-//                in = new BufferedReader(
-//                        new InputStreamReader(pro.getInputStream()));
-//                while ((line = in.readLine()) != null) { //display the output of the java program in console
-//                    rstDis.append(line + "\r\n");
-//                    // System.out.println("java Main" + " The output of this program : " + " " + line);
-//                }
-//                line = null;
-//                in = new BufferedReader(
-//                        new InputStreamReader(pro.getErrorStream()));
-//                while ((line = in.readLine()) != null) { //display the error location when running the java program
-//                    rstDis.append(line + "\r\n");
-//                    // System.out.println("java Main" + " The error : " + " " + line);
-//                }
-//                pro.waitFor();
-//                exitValue = pro.exitValue();
-//                if (exitValue == 1) {
-//                    sss = Integer.toString(50);
-//                    grade(sss);
-//                    gra.setText(sss);
-//                } else {
-//                    sss = Integer.toString(90);
-//                    grade(sss);
-//                    gra.setText(sss);
-//                }
-//                // System.out.println("java Main" + " exitValue() " + pro.exitValue()); //show "0" when running successfully
-//                // read();
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
 
-//    public void run() {
-//        String sss = "";
-//        try {
-//            Process pro = Runtime.getRuntime().exec("g++ Hello.cpp -o Hello"); //need to modify to get the path of the file.
-//            String line = null;
-//            BufferedReader in = new BufferedReader(
-//                    new InputStreamReader(pro.getErrorStream()));
-//            while ((line = in.readLine()) != null) { //display the error location of compiling
-//                rstDis.append(line + "\r\n");
-//                // System.out.println("g++ Hello.cpp" + " The error : " + " " + line);
-//            }
-//            pro.waitFor();
-//            int exitValue = pro.exitValue();
-//
-//            // System.out.println("g++ Hello.cpp" + " exitValue() " + pro.exitValue()); //show "0" if compile successfully
-//
-//            if (exitValue == 1) {
-//                sss = Integer.toString(25);
-//                grade(sss);
-//                gra.setText(sss);
-//            } else {
-//                pro = Runtime.getRuntime().exec("./Hello");
-//                line = null;
-//                in = new BufferedReader(
-//                        new InputStreamReader(pro.getInputStream()));
-//                while ((line = in.readLine()) != null) { //display the output of the java program in console
-//                    rstDis.append(line + "\r\n");
-//                    // System.out.println("run Hello" + " The output of this program : " + " " + line);
-//                }
-//                line = null;
-//                in = new BufferedReader(
-//                        new InputStreamReader(pro.getErrorStream()));
-//                while ((line = in.readLine()) != null) { //display the error location when running the java program
-//                    rstDis.append(line + "\r\n");
-//                    // System.out.println("run Hello" + " The error : " + " " + line);
-//                }
-//                pro.waitFor();
-//                exitValue = pro.exitValue();
-//                if (exitValue == 1) {
-//                    sss = Integer.toString(50);
-//                    grade(sss);
-//                    gra.setText(sss);
-//                } else {
-//                    sss = Integer.toString(90);
-//                    grade(sss);
-//                    gra.setText(sss);
-//                }
-//                // System.out.println("run Hello" + " exitValue() " + pro.exitValue()); //show "0" when running successfully
-//                // read();
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    public void runJava() {
+        String sss = "";
+        try {
+            Process pro = Runtime.getRuntime().exec("cd /Users/yucheng/Desktop/No1");
+            pro.waitFor();
+            pro = Runtime.getRuntime().exec("javac Main.java"); //need to modify to get the path of the file.
+            String line = null;
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(pro.getErrorStream()));
+            while ((line = in.readLine()) != null) { //display the error location of compiling
+                rstDis.append(line + "\r\n");
+                // System.out.println("javac Main.java" + " The error : " + " " + line);
+            }
+            pro.waitFor();
+            int exitValue = pro.exitValue();
+
+                // System.out.println("javac Main.java" + " exitValue() " + pro.exitValue()); //show "0" if compile successfully
+
+            if (exitValue == 1) {
+                sss = Integer.toString(25);
+                grade(sss);
+                gra.setText(sss);
+            } else {
+                pro = Runtime.getRuntime().exec("java Main");
+                line = null;
+                in = new BufferedReader(
+                        new InputStreamReader(pro.getInputStream()));
+                while ((line = in.readLine()) != null) { //display the output of the java program in console
+                    rstDis.append(line + "\r\n");
+                    // System.out.println("java Main" + " The output of this program : " + " " + line);
+                }
+                line = null;
+                in = new BufferedReader(
+                        new InputStreamReader(pro.getErrorStream()));
+                while ((line = in.readLine()) != null) { //display the error location when running the java program
+                    rstDis.append(line + "\r\n");
+                    // System.out.println("java Main" + " The error : " + " " + line);
+                }
+                pro.waitFor();
+                exitValue = pro.exitValue();
+                if (exitValue == 1) {
+                    sss = Integer.toString(50);
+                    grade(sss);
+                    gra.setText(sss);
+                } else {
+                    sss = Integer.toString(90);
+                    grade(sss);
+                    gra.setText(sss);
+                }
+                // System.out.println("java Main" + " exitValue() " + pro.exitValue()); //show "0" when running successfully
+                // read();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void runCpp() {
+        String sss = "";
+        try {
+            Process pro = Runtime.getRuntime().exec("cd /Users/yucheng/Desktop/No1");
+            pro.waitFor();
+            pro = Runtime.getRuntime().exec("g++ Hello.cpp -o Hello"); //need to modify to get the path of the file.
+            String line = null;
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(pro.getErrorStream()));
+            while ((line = in.readLine()) != null) { //display the error location of compiling
+                rstDis.append(line + "\r\n");
+                // System.out.println("g++ Hello.cpp" + " The error : " + " " + line);
+            }
+            pro.waitFor();
+            int exitValue = pro.exitValue();
+
+            // System.out.println("g++ Hello.cpp" + " exitValue() " + pro.exitValue()); //show "0" if compile successfully
+
+            if (exitValue == 1) {
+                sss = Integer.toString(25);
+                grade(sss);
+                gra.setText(sss);
+            } else {
+                pro = Runtime.getRuntime().exec("./Hello");
+                line = null;
+                in = new BufferedReader(
+                        new InputStreamReader(pro.getInputStream()));
+                while ((line = in.readLine()) != null) { //display the output of the java program in console
+                    rstDis.append(line + "\r\n");
+                    // System.out.println("run Hello" + " The output of this program : " + " " + line);
+                }
+                line = null;
+                in = new BufferedReader(
+                        new InputStreamReader(pro.getErrorStream()));
+                while ((line = in.readLine()) != null) { //display the error location when running the java program
+                    rstDis.append(line + "\r\n");
+                    // System.out.println("run Hello" + " The error : " + " " + line);
+                }
+                pro.waitFor();
+                exitValue = pro.exitValue();
+                if (exitValue == 1) {
+                    sss = Integer.toString(50);
+                    grade(sss);
+                    gra.setText(sss);
+                } else {
+                    sss = Integer.toString(90);
+                    grade(sss);
+                    gra.setText(sss);
+                }
+                // System.out.println("run Hello" + " exitValue() " + pro.exitValue()); //show "0" when running successfully
+                // read();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
-    public void run() {
+    public void runPython() {
         String sss = "";
         try {
             Process pro = Runtime.getRuntime().exec("python /Users/yucheng/Desktop/No1/Hi.py");
