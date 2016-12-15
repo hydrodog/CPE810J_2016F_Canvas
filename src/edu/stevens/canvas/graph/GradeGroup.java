317053a9806d652039c460706cb0297ebf89fa06
changlan1030 <lchang1@stevens.edu>
2016-12-15 18:25:15 -0500

    Update
    Add comments, display single student's grade

--- src/edu/stevens/canvas/graph/GradeGroup.java
+++ src/edu/stevens/canvas/graph/GradeGroup.java
@@ -4,28 +4,31 @@ import java.io.*;
 import java.util.*;
 
 /**
- * class to get the grade file
+ * class to get the grade file and number file
  * @author Lan Chang
  *
  */
 
 public class GradeGroup {
 	private static File[] fileList;
-	private boolean allStudent, allAssignment;
-	private String assignmentTypeChoosen, assignment;
 	private static ArrayList<Double> grade;
 	private static ArrayList<Integer> num;
-	private static HashMap<String, Double> multiGrade;
-	private int group;
-	private double fullScore, width, fullScore_temp;
-	private boolean done;
-	private int p1, p2, p3, p4;
+	private String assignmentTypeChoosen;
+	private String assignment;
+	private String student;
+	private StringBuilder sb;
+	private int group, p1, p2, p3, p4;
+	private double fullScore, width, fullScore_temp, fullAssignment, fullQuiz, fullTest, fullProject;
+	private boolean allStudent, allAssignment, done;
+	private HashMap<String, Double> multiGrade, gradeAll, gradeAssignment, gradeQuiz, gradeTest, gradeProject;
 	
-	public GradeGroup(boolean allStudent, boolean allAssignment, String assignmentTypeChoosen, String assignment, int[] percent) {
+	// constructor for calculate
+	public GradeGroup(boolean allStudent, boolean allAssignment, String assignmentTypeChoosen, String assignment, String student, int[] percent) {
 		this.allStudent = allStudent;
 		this.allAssignment = allAssignment;
 		this.assignmentTypeChoosen = assignmentTypeChoosen;
 		this.assignment = assignment;
+		this.student = student;
 		p1 = percent[0];
 		p2 = percent[1];
 		p3 = percent[2];
@@ -34,22 +37,32 @@ public class GradeGroup {
 		File file = new File("grade");
 		fileList = file.listFiles();
 		
+		// all student one assignment
 		if (this.allStudent == true && this.allAssignment == false && !this.assignment.equals("")) {
 			getSingleGradeFile();
 			done = true;
 		}
 		
+		// all student category assignment
 		if (this.allStudent == true && this.allAssignment == true && !assignmentTypeChoosen.equals("All")) {
 			getMultiGradeFile();
 			done = true;
 		}
 		
+		// all student all assignment
 		if (this.allStudent == true && this.allAssignment == true && assignmentTypeChoosen.equals("All")) {
 			getAllGradeFile();
 			done = true;
 		}
+		
+		// single student all assignment
+		if (this.allStudent == false && this.allAssignment == true && assignmentTypeChoosen.equals("All") && !this.student.equals("")) {
+			getStudentFile();
+			done = true;
+		}
 	}
 	
+	// constructor for draw
 	public GradeGroup(boolean allStudent, boolean allAssignment, String assignmentTypeChoosen, String assignment, double width, int[] percent) {
 		this.allStudent = allStudent;
 		this.allAssignment = allAssignment;
@@ -64,18 +77,21 @@ public class GradeGroup {
 		File file = new File("grade");
 		fileList = file.listFiles();
 		
+		// all student one assignment
 		if (this.allStudent == true && this.allAssignment == false && !this.assignment.equals("")) {
 			getSingleGradeFile();
 			cal();
 			done = true;
 		}
 		
+		// all student category assignment
 		if (this.allStudent == true && this.allAssignment == true && !assignmentTypeChoosen.equals("All")) {
 			getMultiGradeFile();
 			cal();
 			done = true;
 		}
 		
+		// all student all assignment
 		if (this.allStudent == true && this.allAssignment == true && assignmentTypeChoosen.equals("All")) {
 			getAllGradeFile();
 			cal();
@@ -83,33 +99,56 @@ public class GradeGroup {
 		}
 	}
 	
+	// get the single student grade file
+	public void getStudentFile() {
+		getAllGradeFile();
+		
+		sb = new StringBuilder("");
+		
+		sb.append("Grade: " + "\r\n" + "\r\n");
+		sb.append("Student: " + student + "\r\n" + "\r\n");
+		sb.append("Assignment: " + gradeAssignment.get(student) / fullAssignment * 100 + "\r\n" + "\r\n");
+		sb.append("Quiz: " + gradeQuiz.get(student) / fullQuiz * 100 + "\r\n" + "\r\n");
+		sb.append("Test: " + gradeTest.get(student) / fullTest * 100 + "\r\n" + "\r\n");
+		sb.append("Project: " + gradeProject.get(student) / fullProject * 100 + "\r\n" + "\r\n");
+		sb.append("All: " + gradeAll.get(student) + "\r\n" + "\r\n");
+	}
+	
+	// get the all student all assignment grade file
 	public void getAllGradeFile() {
+		// get assignment grade file
 		assignmentTypeChoosen = "Assignment";
 		getMultiGradeFile();
-		HashMap<String, Double> gradeAssignment = multiGrade;
-		double fullAssignment = fullScore_temp;
+		gradeAssignment = multiGrade;
+		fullAssignment = fullScore_temp;
 		
+		// get quiz grade file
 		assignmentTypeChoosen = "Quiz";
 		getMultiGradeFile();
-		HashMap<String, Double> gradeQuiz = multiGrade;
-		double fullQuiz = fullScore_temp;
+		gradeQuiz = multiGrade;
+		fullQuiz = fullScore_temp;
 		
+		// get test grade file
 		assignmentTypeChoosen = "Test";
 		getMultiGradeFile();
-		HashMap<String, Double> gradeTest = multiGrade;
-		double fullTest = fullScore_temp;
+		gradeTest = multiGrade;
+		fullTest = fullScore_temp;
 		
+		// get project grade file
 		assignmentTypeChoosen = "Project";
 		getMultiGradeFile();
-		HashMap<String, Double> gradeProject = multiGrade;
-		double fullProject = fullScore_temp;
+		gradeProject = multiGrade;
+		fullProject = fullScore_temp;
 		
-		HashMap<String, Double> gradeAll = new HashMap<String, Double> ();
+		// store all assignment grade file with ID
+		gradeAll = new HashMap<String, Double> ();
 		
+		// assignment part
 		for (HashMap.Entry<String, Double> entry : gradeAssignment.entrySet()) {
 			gradeAll.put(entry.getKey(), entry.getValue() / fullAssignment * p1);
 		}
 		
+		// quiz part
 		for (HashMap.Entry<String, Double> entry : gradeQuiz.entrySet()) {
 			if (!gradeAll.containsKey(entry.getKey())) {
 				gradeAll.put(entry.getKey(), entry.getValue() / fullQuiz * p2);
@@ -119,6 +158,7 @@ public class GradeGroup {
 			}
 		}
 		
+		// test part
 		for (HashMap.Entry<String, Double> entry : gradeTest.entrySet()) {
 			if (!gradeAll.containsKey(entry.getKey())) {
 				gradeAll.put(entry.getKey(), entry.getValue() / fullTest * p3);
@@ -128,6 +168,7 @@ public class GradeGroup {
 			}
 		}
 		
+		// project part
 		for (HashMap.Entry<String, Double> entry : gradeProject.entrySet()) {
 			if (!gradeAll.containsKey(entry.getKey())) {
 				gradeAll.put(entry.getKey(), entry.getValue() / fullProject * p4);
@@ -137,6 +178,7 @@ public class GradeGroup {
 			}
 		}
 		
+		// store all assignment grade file without ID
 		grade = new ArrayList<Double> ();
 		
 		for (HashMap.Entry<String, Double> entry : gradeAll.entrySet()) {
@@ -148,7 +190,9 @@ public class GradeGroup {
 		group = (int) (100 / width);
 	}
 	
+	// get the all student category assignment grade file
 	public void getMultiGradeFile() {
+		// regex for file name
 		String assignmentRegex = "[Aa]ssignment[0-9]*[.txt]*";
 		String projectRegex = "[Pp]roject[0-9]*[.txt]*";
 		String testRegex = "[Tt]est[0-9]*[.txt]*";
@@ -156,6 +200,7 @@ public class GradeGroup {
 		
 		ArrayList<Integer> index = new ArrayList<Integer>();
 		
+		// find the index of assignment we want
 		for (int i = 0; i < fileList.length; i++) {
 			if (fileList[i].getName().matches(assignmentRegex) && assignmentTypeChoosen.matches(assignmentRegex)) {
 				index.add(i);
@@ -173,6 +218,7 @@ public class GradeGroup {
 		
 		fullScore_temp = 0;
 		
+		// store category assignment grade file with ID
 		multiGrade = new HashMap<String, Double> ();
 		
 		for (int i = 0; i < index.size(); i++) {
@@ -199,6 +245,7 @@ public class GradeGroup {
 			}
 		}
 		
+		// store category assignment grade file without ID
 		grade = new ArrayList<Double> ();
 		
 		for (HashMap.Entry<String, Double> entry : multiGrade.entrySet()) {
@@ -210,16 +257,19 @@ public class GradeGroup {
 		group = (int) (100 / width);
 	}
 	
+	// get the all student one assignment grade file
 	public void getSingleGradeFile() {
 		int index = -1;
 		String filename = assignment + ".txt";
 		
+		// get the file index
 		for (int i = 0; i < fileList.length; i++) {
 			if (filename.equals(fileList[i].getName())) {
 				index = i;
 			}
 		}
 		
+		// store one assignment grade file without ID
 		grade = new ArrayList<Double> ();
 		
 		Scanner fileInput;
@@ -238,12 +288,14 @@ public class GradeGroup {
 		}
 	}
 	
+	// calculate the number of student in each grade group
 	public void cal() {		
 		num = new ArrayList<Integer> ();
 		
 		for (int i = 0; i < group; i++) {
 			num.add(0);
 		}
+		
 		for (int i = 0; i < grade.size(); i++) {
 			if (grade.get(i) != fullScore) {
 				num.set((int) (grade.get(i) / width), num.get((int) (grade.get(i) / width)) + 1);
@@ -254,19 +306,28 @@ public class GradeGroup {
 		}
 	}
 	
+	// return the number of student in each group array list
 	public ArrayList<Integer> getNum() {
 		return num;
 	}
 	
+	// return the grade array list
 	public ArrayList<Double> getGrade() {
 		return grade;
 	}
 	
+	// return if it is done
 	public boolean getDone() {
 		return done;
 	}
 	
+	// return the full score
 	public double getFull() {
 		return fullScore;
+	}
+	
+	// return the single student's grade
+	public StringBuilder getStr() {
+		return sb;
 	}
 }
